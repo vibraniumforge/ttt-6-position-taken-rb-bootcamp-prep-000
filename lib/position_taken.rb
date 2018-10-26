c886cbd35cb84776701855d86281b2afa8020541
Greg Morgan <gregm9999@gmail.com>
2018-10-26 01:14:47 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,5 +5,9 @@ def position_taken?(board, index)
     return false
   elsif board[index] ==""
       return false
+  elsif board[index]==nil
+    return false
+  elsif board[index] == "X" || "O"
+  return true
 end
 end
